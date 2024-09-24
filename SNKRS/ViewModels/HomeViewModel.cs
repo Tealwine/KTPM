5bf863c127fc4234eac3b5c0b082edcaf2eab552
Tealwine <ngomuimac@gmail.com>
2024-09-24 14:56:28 +0700

    Update

---
+++ SNKRS/ViewModels/HomeViewModel.cs
@@ -0,0 +1,12 @@
+﻿using SNKRS.Models;
+using System.Collections.Generic;
+
+namespace SNKRS.ViewModels
+{
+    public class HomeViewModel
+    {
+        public IEnumerable<Portfolio> Upcoming { get; set; }
+        public IEnumerable<Portfolio> Best_selling { get; set; }
+        public IEnumerable<Portfolio> OnSale { get; set; }
+    }
+}
---
+++ SNKRS/ViewModels/PortfolioViewModel.cs
@@ -0,0 +1,36 @@
+﻿using SNKRS.Models;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+namespace SNKRS.ViewModels
+{
+    public class PortfolioViewModel
+    {
+		public int Id { get; set; }
+
+		[Required]
+		public string Name { get; set; }
+
+
+		[AllowHtml]
+		public string Description { get; set; }
+
+		[Required]
+		public string Image { get; set; }
+
+		[Display(Name = "Product Visibility")]
+		public bool isVisible { get; set; }
+
+		public IEnumerable<Category> Categories { get; set; }
+		public IEnumerable<int> ProductCategories { get; set; }
+		public IEnumerable<string> ProductCategoryNames { get; set; }
+		public IEnumerable<ProductGallery> ProductGalleries { get; set; }
+		public Portfolio portfolio { get; set; }
+
+		public PortfolioViewModel()
+		{
+			portfolio = new Portfolio(); // Initialize the portfolio property
+		}
+	}
+}
